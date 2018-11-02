fc7283d972223d4748b2908cf931cc4fe029d32e
Michael Jacobsen <michael@visbyjacobsen.dk>
2018-11-02 19:26:43 +0100

    Initital

---
+++ google-smarthome.js
@@ -0,0 +1,28 @@
+/**
+ * NodeRED Google SmartHome
+ * Copyright (C) 2018 Michael Jacobsen.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+
+module.exports = function(RED) {
+    "use strict";
+
+    const SmartHomeApp = require('actions-on-google').SmartHomeApp;
+    const express      = require('express');
+    const https        = require("https");
+    const fs           = require('fs');
+    const bodyParser   = require('body-parser');
+}
