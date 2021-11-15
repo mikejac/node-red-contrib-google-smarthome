0b6de45972a583ee6cc00753af01c189cdfd4435
Caprico85 <Caprico85@users.noreply.github.com>
2021-11-15 18:52:29 +0100

    Change all Open/Close devices to use base device internally

--- devices/window.js
+++ devices/window.js
@@ -16,223 +16,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
+const BaseDevice = require('./baseDevice');
+
 module.exports = function(RED) {
     "use strict";
 
-    const formats = require('../formatvalues.js');
-    const util    = require('util');
+    class WindowNode extends BaseDevice {
 
-    /******************************************************************************************************************
-     *
-     *
-     */
-    class WindowNode {
         constructor(config) {
-            RED.nodes.createNode(this, config);
+            config.device_type = 'WINDOW';
+            config.trait_openclose = true;
 
-            this.client     = config.client;
-            this.clientConn = RED.nodes.getNode(this.client);
-            this.topicOut   = config.topic;
-            this.room_hint  = config.room_hint;
-            this.passthru   = config.passthru;
-            this.topicDelim = '/';
+            config.discrete_only_openclose = false;
+            config.open_direction = [];
+            config.command_only_openclose = false;
+            config.query_only_openclose = false;
 
-            if (!this.clientConn) {
-                this.error(RED._("window.errors.missing-config"));
-                this.status({fill: "red", shape: "dot", text: "Missing config"});
-                return;
-            } else if (typeof this.clientConn.register !== 'function') {
-                this.error(RED._("window.errors.missing-bridge"));
-                this.status({fill: "red", shape: "dot", text: "Missing SmartHome"});
-                return;
-            }
-
-            this.states = this.clientConn.register(this, 'window', config.name);
-
-            this.status({fill: "yellow", shape: "dot", text: "Ready"});
-
-            this.on('input', this.onInput);
-            this.on('close', this.onClose);
-        }
-
-        /******************************************************************************************************************
-         * called to register device
-         *
-         */
-        registerDevice(client, name) {
-            let states = {
-                online: true,
-                openPercent: 0
-            };
-
-            let device = {
-                id: client.id,
-                properties: {
-                    type: 'action.devices.types.WINDOW',
-                    traits: ['action.devices.traits.OpenClose'],
-                    name: {
-                        defaultNames: ["Node-RED Window"],
-                        name: name
-                    },
-                    roomHint: this.room_hint,
-                    willReportState: true,
-                    attributes: {
-                    },
-                    deviceInfo: {
-                        manufacturer: 'Node-RED',
-                        model: 'nr-window-v1',
-                        swVersion: '1.0',
-                        hwVersion: '1.0'
-                    }
-                }
-            };
-
-            device.states = states;
-
-            return device;
-        }
-
-        updateStatusIcon() {
-            if (this.states.openPercent === 0) {
-                this.status({fill: "green", shape: "dot", text: "CLOSED"});
-            } else {
-                this.status({fill: "red", shape: "dot", text: util.format("OPEN %d%%", this.states.openPercent)});
-            }
-        }
-
-        /******************************************************************************************************************
-         * called when state is updated from Google Assistant
-         *
-         */
-        updated(device) {
-            let states = device.states;
-            let command = device.command;
-            RED.log.debug("WindowNode(updated): states = " + JSON.stringify(states));
-
-            Object.assign(this.states, states);
-
-            this.updateStatusIcon();
-
-            let msg = {
-                topic: this.topicOut,
-                device_name: device.properties.name.name,
-                command: command,
-                payload: {
-                    online: states.online,
-                    openPercent: states.openPercent,
-                },
-            };
-
-            this.send(msg);
-        };
-
-        /******************************************************************************************************************
-         * respond to inputs from NodeRED
-         *
-         */
-        onInput(msg) {
-            RED.log.debug("WindowNode(input)");
-
-            let topicArr = String(msg.topic).split(this.topicDelim);
-            let topic    = topicArr[topicArr.length - 1];   // get last part of topic
-
-            RED.log.debug("WindowNode(input): topic = " + topic);
-
-            try {
-                if (topic.toUpperCase() === 'OPENPERCENT') {
-                    RED.log.debug("WindowNode(input): OPEN/OPENPERCENT");
-                    let openPercent;
-                    if(msg.payload.toString().toUpperCase() === 'TRUE')
-                        openPercent = 100;
-                    else if(msg.payload.toString().toUpperCase() === 'FALSE')
-                        openPercent = 0;
-                    else
-                        openPercent = formats.FormatValue(formats.Formats.INT, 'openPercent', msg.payload);
-
-                    if (this.states.openPercent !== openPercent) {
-                        this.states.openPercent = openPercent;
-
-                        this.clientConn.setState(this, this.states);  // tell Google ...
-
-                        if (this.passthru) {
-                            msg.payload = this.states.openPercent;
-                            this.send(msg);
-                        }
-
-                        this.updateStatusIcon();
-                    }
-                } else if (topic.toUpperCase() === 'ONLINE') {
-                    RED.log.debug("WindowNode(input): ONLINE");
-                    let online = formats.FormatValue(formats.Formats.BOOL, 'online', msg.payload);
-
-                    if (this.states.online !== online) {
-                        this.states.online = online;
-
-                        this.clientConn.setState(this, this.states);  // tell Google ...
-
-                        if (this.passthru) {
-                            msg.payload = this.states.online;
-                            this.send(msg);
-                        }
-                    }
-                } else {
-                    RED.log.debug("WindowNode(input): some other topic");
-                    let object = {};
-
-                    if (typeof msg.payload === 'object') {
-                        object = msg.payload;
-                    } else {
-                        RED.log.debug("WindowNode(input): typeof payload = " + typeof msg.payload);
-                        return;
-                    }
-
-                    let openPercent     = this.states.openPercent;
-                    let online = this.states.online;
-
-                    // openPercent
-                    if (object.hasOwnProperty('openPercent')) {
-                        if(object.openPercent.toString().toUpperCase() === 'TRUE')
-                            openPercent = 100;
-                        else if(object.openPercent.toString().toUpperCase() === 'FALSE')
-                            openPercent = 0;
-                        else
-                            openPercent = formats.FormatValue(formats.Formats.INT, 'openPercent', object.openPercent);
-                    }
-
-                    // online
-                    if (object.hasOwnProperty('online')) {
-                        online = formats.FormatValue(formats.Formats.BOOL, 'online', object.online);
-                    }
-
-                    if (this.states.openPercent !== openPercent || this.states.online !== online){
-                        this.states.openPercent     = openPercent;
-                        this.states.online = online;
-
-                        this.clientConn.setState(this, this.states);  // tell Google ...
-
-                        if (this.passthru) {
-                            msg.payload = this.states;
-                            this.send(msg);
-                        }
-
-                        this.updateStatusIcon();
-                    }
-                }
-            } catch (err) {
-                RED.log.error(err);
-            }
-        }
-
-        onClose(removed, done) {
-            if (removed) {
-                // this node has been deleted
-                this.clientConn.remove(this, 'window');
-            } else {
-                // this node is being restarted
-                this.clientConn.deregister(this, 'window');
-            }
-
-            done();
+            super(config, RED);
         }
     }
 
