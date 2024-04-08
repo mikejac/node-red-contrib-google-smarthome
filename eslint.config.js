b1e1b8a29f9dbb3140497381c00965404e6638ed
Caprico85 <Caprico85@users.noreply.github.com>
2024-04-08 22:28:50 +0200

    New config for eslint 9.0.0

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+const js = require('@eslint/js');
+const jsdoc = require('eslint-plugin-jsdoc'); 
+const globals = require('globals'); 
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+    js.configs.recommended,
+    jsdoc.configs['flat/recommended'],
+    {
+        files: ["**/*.js", "**/*.html"],
+        languageOptions: {
+            globals: {
+                ...globals.es6,
+                ...globals.node,
+                ...globals.mocha,
+                RED: "readonly"
+            },
+            sourceType: "commonjs"
+        },
+        plugins: {
+            jsdoc: jsdoc,
+        },
+        settings: {
+            jsdoc: {
+                mode: "typescript"
+            }
+        },
+        rules: {
+            "indent": ["error", 4, {
+                "SwitchCase": 1
+            }],
+            "jsdoc/require-hyphen-before-param-description": 1,
+            "jsdoc/tag-lines": ["error", "any", {
+                "startLines": 1
+            }]
+        }
+    }
+];
